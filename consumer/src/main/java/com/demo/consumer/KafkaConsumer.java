c34cc38f902ab48c64d588886f40a46d868e852b
Habib-hs <mhabibullahhs@gmail.com>
2025-07-18 17:16:02 +0600

    deserialization object set up

--- consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -14,4 +14,11 @@ public class KafkaConsumer {
     public void listen2(String message) {
         System.out.println("Received message from consumer 2: " + message);
     }
+
+    @KafkaListener(topics = "topic_from_bin_1", groupId = "my-group-new3-bin1")
+    public void listenRiderLocation(RiderLocation location) {
+        System.out.println("Received Location : " + location.getRiderId() +
+                           " at latitude: " + location.getLatitude() +
+                           ", longitude: " + location.getLongitude());
+    }
 }
