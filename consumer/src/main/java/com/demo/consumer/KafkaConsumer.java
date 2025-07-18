768a23b60fab71b1c119088976b806d68db22963
Habib-hs <mhabibullahhs@gmail.com>
2025-07-18 15:49:42 +0600

    multiple consumer added for same topic

--- consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
+++ consumer/src/main/java/com/demo/consumer/KafkaConsumer.java
@@ -5,8 +5,13 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaConsumer {
-    @KafkaListener(topics = "my-topic", groupId = "my-group-new")
-    public void listen(String message) {
-        System.out.println("Received message: " + message);
+    @KafkaListener(topics = "my-topic", groupId = "my-group-new1")
+    public void listen1(String message) {
+        System.out.println("Received message from consumer 1: " + message);
+    }
+
+    @KafkaListener(topics = "my-topic", groupId = "my-group-new2")
+    public void listen2(String message) {
+        System.out.println("Received message from consumer 2: " + message);
     }
 }
